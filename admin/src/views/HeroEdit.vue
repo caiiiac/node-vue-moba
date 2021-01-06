aa251cbaa6e8898674d2514c0158104f0ea942e0
caijq <caiiiac@163.com>
2021-01-06 16:10:48 +0800

    英雄编辑

--- admin/src/views/HeroEdit.vue
+++ admin/src/views/HeroEdit.vue
@@ -2,8 +2,19 @@
   <div>
     <h1>{{id ? '编辑' : '新建'}}英雄</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
-      <el-form-item label="名称">
+      <el-tabs value="basic" type="border-card">
+        <el-tab-pane label="基本信息" name="basic">
+          <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
+      </el-form-item>
+      <el-form-item label="称号">
+        <el-input v-model="model.title"></el-input>
+      </el-form-item>
+      <el-form-item label="职业">
+        <el-select v-model="model.categories" multiple>
+          <el-option v-for="item of categories" :key="item._id"
+            :label="item.name" :value="item._id" ></el-option>
+        </el-select>
       </el-form-item>
       <el-form-item label="头像">
         <el-upload
@@ -16,7 +27,45 @@
           <i v-else class="el-icon-plus avatar-uploader-icon"></i>
         </el-upload>
       </el-form-item>
-      <el-form-item>
+      <el-form-item label="难度">
+        <el-rate style="margin-top:0.6rem" :max="10" show-score v-model="model.scores.difficult"></el-rate>
+      </el-form-item>
+      <el-form-item label="技能">
+        <el-rate style="margin-top:0.6rem" :max="10" show-score v-model="model.scores.skills"></el-rate>
+      </el-form-item>
+      <el-form-item label="攻击">
+        <el-rate style="margin-top:0.6rem" :max="10" show-score v-model="model.scores.attack"></el-rate>
+      </el-form-item>
+      <el-form-item label="生存">
+        <el-rate style="margin-top:0.6rem" :max="10" show-score v-model="model.scores.survive"></el-rate>
+      </el-form-item>
+      <el-form-item label="顺风出装">
+        <el-select v-model="model.items1" multiple>
+          <el-option v-for="item of items" :key="item._id"
+            :label="item.name" :value="item._id"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="逆风出装">
+        <el-select v-model="model.items2" multiple>
+          <el-option v-for="item of items" :key="item._id"
+            :label="item.name" :value="item._id"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="使用技巧">
+        <el-input type="textarea" v-model="model.usageTips"></el-input>
+      </el-form-item>
+      <el-form-item label="对抗技巧">
+        <el-input type="textarea" v-model="model.battleTips"></el-input>
+      </el-form-item>
+      <el-form-item label="团战思路">
+        <el-input type="textarea" v-model="model.teamTips"></el-input>
+      </el-form-item>
+        </el-tab-pane>
+        <el-tab-pane label="技能" name="skills">
+
+        </el-tab-pane>
+      </el-tabs>
+      <el-form-item style="margin-top: 1rem;">
         <el-button type="primary" native-type="submit">保存</el-button>
       </el-form-item>
     </el-form>
@@ -30,9 +79,17 @@ export default {
   },
     data () {
         return {
+          categories: [],
+          items: [],
             model: {
               name: '',
-              avatar: ''
+              avatar: '',
+              scores: {
+                difficult: 0,
+                skills: 0,
+                attack: 0,
+                survive: 0
+              }
             },
         }
     },
@@ -52,15 +109,25 @@ export default {
           this.$message({
               type: 'success',
               message: '保存成功'
-          })
+          });
           console.log(res)
       },
       async fetch() {
         const res = await this.$http.get(`rest/heros/${this.id}`)
-        this.model = res.data
+        this.model = Object.assign({}, this.model, res.data)
+      },
+      async fetchCategories() {
+        const res = await this.$http.get(`rest/categories`)
+        this.categories = res.data;
+      },
+      async fetchItems() {
+        const res = await this.$http.get(`rest/items`)
+        this.items = res.data;
       },
     },
     created () {
+      this.fetchCategories()
+      this.fetchItems()
       this.id && this.fetch()
     }
 }
