0f9572b38bfc86dee538d87d473db54c6338e17d
caijq <caiiiac@163.com>
2021-01-07 16:54:14 +0800

    富文本编辑器

--- admin/src/views/ArticleEdit.vue
+++ admin/src/views/ArticleEdit.vue
@@ -4,15 +4,19 @@
     <el-form label-width="120px" @submit.native.prevent="save">
       <el-form-item label="所属分类">
         <el-select v-model="model.categories" multiple>
-          <el-option v-for="item in categories" :key="item._id"
-            :label="item.name" :value="item._id"></el-option>
+          <el-option
+            v-for="item in categories"
+            :key="item._id"
+            :label="item.name"
+            :value="item._id"
+          ></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="标题">
         <el-input v-model="model.title"></el-input>
       </el-form-item>
       <el-form-item label="详情">
-        <el-input type="textarea" v-model="model.body"></el-input>
+        <vue-editor v-model="model.body"></vue-editor>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
@@ -22,10 +26,16 @@
 </template>
 
 <script>
+
+import { VueEditor } from 'vue2-editor'
+
 export default {
   props: {
     id: {}
   },
+  components: {
+    VueEditor
+  }, 
     data () {
         return {
             model: {},
