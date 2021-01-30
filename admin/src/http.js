3cac92623c41719b17eb87a5654a394f0f76e8e2
caijq <caiiiac@163.com>
2021-01-30 16:31:13 +0800

    身份验证+错误处理

--- admin/src/http.js
+++ admin/src/http.js
@@ -1,10 +1,20 @@
 import axios from 'axios'
 import Vue from 'vue'
+import router from './router'
 
 const http = axios.create({
     baseURL: "http://localhost:3002/admin/api"
 })
 
+http.interceptors.request.use(function (config) {
+    if (localStorage.token) {
+        config.headers.Authorization = 'Bearer ' + (localStorage.token || '')
+    }
+
+    return config
+}, function(error) {
+    return Promise.reject(error)
+})
 
 http.interceptors.response.use(res => {
     return res
@@ -14,6 +24,9 @@ http.interceptors.response.use(res => {
             type: 'error',
             message: err.response.data.message
         })
+        if (err.response.status === 401) {
+            router.push('/login')
+        }
     }
 
     return Promise.reject(err)
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,17 +1,26 @@
 module.exports = app => {
     const express = require('express')
-    const router = express.Router()
+    const jwt = require('jsonwebtoken')
+    const assert = require('http-assert')
+    const AdminUser = require('../../models/AdminUser')
 
+    const router = express.Router({
+        mergeParams: true
+    })
+
+    // 创建资源
     router.post('/', async (req, res) => {
         const model = await req.Model.create(req.body)
         res.send(model)
     })
 
+    // 更新资源
     router.put('/:id', async (req, res) => {
         const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)
         res.send(model)
     })
 
+    // 删除资源
     router.delete('/:id', async (req, res) => {
         const model = await req.Model.findByIdAndDelete(req.params.id, req.body)
         res.send({
@@ -19,7 +28,18 @@ module.exports = app => {
         })
     })
 
-    router.get('/', async (req, res) => {
+    // 资源列表
+    router.get('/', async (req, res, next) => {
+        const token = String(req.headers.authorization || '').split(' ').pop()
+        assert(token, 401, '请提供 token')
+
+        const { id } = jwt.verify(token, app.get('secret'))
+        assert(id, 401, ' 无效的 token')
+
+        req.user = await AdminUser.findById(id)
+        assert(req.user, 401, '请先登录')
+        await next()
+    } , async (req, res) => {
         const queryOptions = {}
         if (req.Model.modelName === 'Category') {
             queryOptions.populate = 'parent'
@@ -30,6 +50,7 @@ module.exports = app => {
         res.send(items)
     })
 
+    // 资源详情
     router.get('/:id', async (req, res) => {
         const model = await req.Model.findById(req.params.id)
         res.send(model)
@@ -58,27 +79,36 @@ module.exports = app => {
         // 查找用户
         const user = await AdminUser.findOne({ username }).select('+password')
 
-        if (!user) {
-            return res.status(422).send({
-                message: '用户不存在'
-            })
-        }
+        assert(user, 422, '用户不存在')
+        // if (!user) {
+        //     return res.status(422).send({
+        //         message: '用户不存在'
+        //     })
+        // }
 
         // 校验密码
         const isValid = require('bcrypt').compareSync(password, user.password)
 
-        if (!isValid) {
-            return res.status(422).send({
-                message: '密码错误'
-            })
-        }
+        assert(isValid, 422, '密码错误')
+        // if (!isValid) {
+        //     return res.status(422).send({
+        //         message: '密码错误'
+        //     })
+        // }
 
-        const jwt = require('jsonwebtoken')
+        // 返回 token
         const token = jwt.sign({ id: user._id}, app.get('secret'))
 
         res.send({ 
             'username': user.username,
             'token': token
          })
+    })
+
+    // 错误处理函数
+    app.use(async (err, req, res, next) => {
+        res.status(err.statusCode || 500).send({
+            message: err.message
+        })
     })
 }
