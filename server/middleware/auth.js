73d33e0826dd9317159520bae963bc29aae16600
caijq <caiiiac@163.com>
2021-02-02 16:29:29 +0800

    优化token验证

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,14 +5,20 @@ module.exports = options => {
     const AdminUser = require('../models/AdminUser')
 
     return async (req, res, next) => {
-        const token = String(req.headers.authorization || '').split(' ').pop()
-        assert(token, 401, '请先登录')
+        
+        try {
+            const token = String(req.headers.authorization || '').split(' ').pop()
+            assert(token, 401, '请先登录')
 
-        const { id } = jwt.verify(token, req.app.get('secret'))
-        assert(id, 401, '请先登录')
+            const { id } = jwt.verify(token, req.app.get('secret'))
+            assert(id, 401, '请先登录')
 
-        req.user = await AdminUser.findById(id)
-        assert(req.user, 401, '请先登录')
-        await next()
+            req.user = await AdminUser.findById(id)
+            assert(req.user, 401, '请先登录')
+            await next()
+
+        } catch (error) {  
+            assert(false, 401, '请先登录')
+        }
     }
 }
