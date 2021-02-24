90cd9091dbed3c57740a4f502463f6ef158887d4
caijq <caiiiac@163.com>
2021-02-24 17:32:57 +0800

    swiper

--- web/src/main.js
+++ web/src/main.js
@@ -6,6 +6,11 @@ Vue.config.productionTip = false
 import './assets/scss/style.scss'
 import router from './router'
 
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'vue-awesome-swiper/node_modules/swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
+
+
 new Vue({
   router,
   render: h => h(App)
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,15 +1,47 @@
 <template>
   <div>
-    Home
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/1526626245389_thumb_216x120.jpg">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/1526692750969_thumb_216x120.jpg">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/1526626245389_thumb_216x120.jpg">
+      </swiper-slide>
+      <div class="swiper-pagination pagination-home text-right px-3 pb-2" slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".pagination-home"
+        }
+      }
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '../assets/scss/variables';
+
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 1;
+    border-radius: 0.1538rem;
+    background: map-get($map: $colors, $key: 'white');
+
+    &.swiper-pagination-bullet-active {
+      background: map-get($map: $colors, $key: 'info');
+    }
+  }
+}
 
 </style>
