57fa643d30a48c123970874b024fa75375e78a60
caijq <caiiiac@163.com>
2021-03-05 17:25:38 +0800

    #web# 英雄详情顶部 ui

--- web/src/views/Hero.vue
+++ web/src/views/Hero.vue
@@ -8,7 +8,26 @@
       </div>
       <router-link to="/" tag="div">更多英雄 &gt;</router-link>
     </div>
-    <h1>{{id}}</h1>
+    <div class="top" :style="{'background-image': `url(${model.banner})`}">
+      <div class="info text-white p-2 h-100 d-flex flex-column jc-end">
+        <div class="fs-sm">{{model.title}}</div>
+        <h2 class="my-2">{{model.name}}</h2>
+        <div class="fs-sm">{{model.categories.map(el => el.name).join('/')}}</div>
+        <div class="d-flex jc-between">
+          <div class="scores d-flex ai-center" v-if="model.scores">
+            <span>难度</span>
+            <span class="badge bg-primary">{{model.scores.difficult}}</span>
+            <span>技能</span>
+            <span class="badge bg-primary">{{model.scores.skills}}</span>
+            <span>攻击</span>
+            <span class="badge bg-primary">{{model.scores.attack}}</span>
+            <span>生存</span>
+            <span class="badge bg-primary">{{model.scores.survive}}</span>
+          </div>
+          <router-link to="/" tag="span" class="text-grey fs-sm">更多皮肤</router-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -34,3 +53,29 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.page-hero {
+  .top {
+    height: 50vw;
+    background: #fff no-repeat top center;
+    background-size: auto 100%;
+  }
+
+  .info {
+    background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,1));
+    .scores {
+      .badge {
+        margin: 0 0.25rem;
+        width: 1rem;
+        height: 1rem;
+        line-height: 1rem;
+        text-align: center;
+        border-radius: 50%;
+        font-size: 0.6rem;
+        border: 1px solid rgba(255,255,255,0.2);
+      }
+    }
+  }
+}
+</style>
